9ceb910a1f86be3bd171a12522f20ccd62ac773a
Richard Diehl Martinez <richarddiehl@gmail.com>
2024-01-17 15:38:05 +0000

    adding opt model class and config

---
+++ src/models/opt.py
@@ -0,0 +1,7 @@
+from .registry import register_model
+from transformers import OPTConfig 
+from transformers import OPTModel as _OPTModel
+
+@register_model("opt", OPTConfig)
+class OPTModel(_OPTModel):
+    pass
