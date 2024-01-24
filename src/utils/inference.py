2b1ebe3c24f8eba94ebdfc67b9e3ba126a204a53
Richard Diehl Martinez <richarddiehl@gmail.com>
2024-01-24 16:20:41 +0000

    generalizing ppl evaluation to also work for causal models

--- src/utils/inference.py
+++ src/utils/inference.py
@@ -16,27 +16,6 @@ if TYPE_CHECKING:
     from src.trainer import CustomTrainer
 
 
-def prepare_dataset_for_ppl_inference(
-    trainer: CustomTrainer,
-    dataset: Dataset,
-) -> Dataset:
-    """
-    Preprocess dataset to remove columns that are not used by the perplexity computation's
-    forward pass through the trainer model.
-
-    Args:
-        * trainer: a trainer object that was used for training the model
-        * dataset: the dataset that will be scored for perplexity
-    """
-    ignore_columns = trainer._get_ignore_columns(dataset)
-    # NOTE: ignore columns should contain special_tokens_mask because these are
-    # always returned by the fast pretrained tokenizer
-    assert "special_tokens_mask" in ignore_columns
-    ignore_columns.remove("special_tokens_mask")
-
-    return dataset.remove_columns(ignore_columns)
-
-
 def compute_trainer_perplexity(
     batch: Dict[str, torch.Tensor],
     tokenizer: PreTrainedTokenizerFast,
@@ -45,8 +24,105 @@ def compute_trainer_perplexity(
 ) -> List[float]:
     """
 
-    A helper fucntion for computing the perplexity of a batch of data. Assumes that the data
+    A helper function for computing the perplexity of a batch of data. Assumes that the data
     has been tokenized and batched using the same tokenizer that was used for training the model.
+
+    Uses either the causal language modeling objective or the masked language modeling objective
+    depending on the model type.
+
+    Args:
+        * batch: a batch of data that should contain a key "input_ids" as well as
+            "special_tokens_mask" (by default this is returned by the tokenizer we uses).
+        * tokenizer: a tokenizer object that was used for tokenizing the input ids, we use this
+            only to determine the mask token id.
+        * trainer: a trainer object that was used for training the model
+        * output_per_token_loss: if True, the function will return a tuple of 
+            (perplexity, token_loss)
+    Returns:
+        * perplexity (float): The perplexity of the n-gram
+
+    """
+
+    if trainer.model.MODEL_TYPE == "encoder":
+        return compute_trainer_perplexity_mlm(
+            batch, tokenizer, trainer, output_per_token_loss
+        )
+    else:
+        return compute_trainer_perplexity_causal(
+            batch, tokenizer, trainer, output_per_token_loss
+        )
+
+def compute_trainer_perplexity_causal(
+    batch: Dict[str, torch.Tensor],
+    tokenizer: PreTrainedTokenizerFast,
+    trainer: CustomTrainer,
+    output_per_token_loss = False,
+) -> List[float]: 
+    """
+    The causal version of the perplexity computation.
+
+    Args:
+        * batch: a batch of data that should contain a key "input_ids" as well as
+            "special_tokens_mask" (by default this is returned by the tokenizer we uses).
+        * tokenizer: a tokenizer object that was used for tokenizing the input ids, we use this
+            only to determine the mask token id.
+        * trainer: a trainer object that was used for training the model
+        * output_per_token_loss: if True, the function will return a tuple of 
+            (perplexity, token_loss)
+    """
+    input_ids = batch["input_ids"].to(trainer.args.device)
+
+    special_tokens_mask = (
+        batch["special_tokens_mask"].to(trainer.args.device)
+    )
+
+    labels = input_ids.clone()
+
+    if tokenizer.pad_token_id is not None:
+        labels[labels == tokenizer.pad_token_id] = -100
+   
+    loss = trainer.objective_task.compute_loss(
+        trainer.model,
+        {"attention_mask": batch["attention_mask"].to(trainer.args.device)},  
+        override_input_ids=input_ids,
+        override_lables=labels,
+        loss_kwargs={
+            "reduction": "none",
+        },
+    )
+
+    # loss is a tensor (batch , seq_len), 
+    loss = loss.sum(dim=-1)
+
+    # Now we divide by the number of non-masked tokens in each batch to get avg loss
+    non_masked_tokens = torch.sum(special_tokens_mask == 0, dim=-1).squeeze()
+
+    # Avoiding division by zero
+    non_masked_tokens[non_masked_tokens == 0] = 1
+
+    mean_loss = loss / non_masked_tokens
+
+    # batch perplexity is a vector of length batch_size
+    batch_perplexity = torch.exp(mean_loss)
+
+    # converting batch perplexity to a list of floats
+    batch_perplexity = batch_perplexity.cpu().tolist()
+
+    if output_per_token_loss:
+        return batch_perplexity, loss.cpu().tolist()
+    else:
+        return batch_perplexity
+
+
+def compute_trainer_perplexity_mlm(
+    batch: Dict[str, torch.Tensor],
+    tokenizer: PreTrainedTokenizerFast,
+    trainer: CustomTrainer,
+    output_per_token_loss = False,
+) -> List[float]:
+    """
+
+    The MLM version of the perplexity computation. 
 
     Args:
         * batch: a batch of data that should contain a key "input_ids" as well as
@@ -88,6 +164,8 @@ def compute_trainer_perplexity(
     labels = repeat_ids.masked_fill(masked_input != mask_idx, -100)
 
     # For each batch, if the label is a special token, set it to -100 (ignore_index)
+    # Check that this isn't messing up the special tokens logic 
+
     special_tokens_mask = (
         batch["special_tokens_mask"].unsqueeze(1).to(trainer.args.device)
     )
@@ -98,22 +176,9 @@ def compute_trainer_perplexity(
     masked_input = masked_input.view(-1, seq_len)
     labels = labels.view(-1, seq_len)
 
-    # NOTE: The 'mlm' unit is always in the objective curriculum
-    # (this is checked by ObjectiveCurriculum.__init__)
-    if "mlm" in trainer.objective_curriculum.units:
-        inference_unit = trainer.objective_curriculum.units["mlm"]
-    elif "pos_merge" in trainer.objective_curriculum.units:
-        inference_unit = trainer.objective_curriculum.units["pos_merge"]
-    else:
-        # NOTE: This should never happen because we check for this in the
-        # ObjectiveCurriculum.__init__ method
-        raise ValueError(
-            "The objective curriculum must have either the 'mlm' or 'pos_merge' unit"
-        )
-
-    loss = inference_unit.compute_loss(
+    loss = trainer.objective_task.compute_loss(
         trainer.model,
-        {},  # We don't provide a standard batch of data
+        {"attention_mask": batch["attention_mask"].to(trainer.args.device)}, 
         override_input_ids=masked_input,
         override_lables=labels,
         loss_kwargs={
