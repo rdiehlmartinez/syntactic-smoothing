e50f419f4960f655652572ce82adf30994800c00
Richard Diehl Martinez <richarddiehl@gmail.com>
2024-01-24 16:16:39 +0000

    minor fix to opt model - incorrect import

--- src/models/opt.py
+++ src/models/opt.py
@@ -1,7 +1,7 @@
 from .registry import register_model
 from transformers import OPTConfig 
 from transformers import OPTModel as _OPTModel
-from transformers import OPTModelForCausalLM as _OPTModelForCausalLM
+from transformers import OPTForCausalLM as _OPTModelForCausalLM
 
 @register_model("opt", OPTConfig)
 class OPTModel(_OPTModel):
